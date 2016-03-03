22f99fcc2805930cc4bb990ae52c7c209c6caece
Josh Street <jstreet@fairfaxmedia.com.au>
2016-03-03 14:14:25 +1100

    Progress bar :D

--- main.js
+++ main.js
@@ -4,15 +4,16 @@
 var fs = require('fs');
 
 var intersect = require('turf-intersect');
-var postcodes = JSON.parse(fs.readFileSync('postcodes.geojson', 'utf8', function(err) {
-  if (err) return console.error('Postcodes failed to load. Please check postcodes.geojson is in the application root.');
-}));
+var postcodes = JSON.parse(fs.readFileSync('postcodes.geojson', 'utf8'));
+var ProgressBar = require('progress');
 
 console.log('Postcodes loaded');
 
 var adjacentpostcodes = {};
 
-var shapes = postcodes.features.slice(0,100);
+var shapes = postcodes.features.slice(0,50);
+var bar = new ProgressBar('[:bar :percent] Elapsed: :elapseds Remaining: :etas', { total: shapes.length });
+
 shapes.map(function(item) {
   var postcode = getPC(item);
   var adjacent = shapes.filter(function(candidate) {
@@ -23,12 +24,13 @@ shapes.map(function(item) {
   }).map(getPC);
 
   adjacentpostcodes[postcode] = adjacent;
+  bar.tick();
 });
 
 fs.writeFile('adjacentpostcodes.json', JSON.stringify(adjacentpostcodes), function(err) {
   if (err) return console.error('An error occured writing the results file');
   console.log('Output saved as adjacentpostcodes.json');
-})
+});
 
 function compareIntersection(source, candidate) {
   return !!intersect(source, candidate) &&
